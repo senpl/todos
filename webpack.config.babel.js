2c5211675eb8732ca001e28c22f265e297c46967
Michał Urbanek <m.urbanek@devesoft.pl>
2018-11-07 16:17:02 +0100

    my webpack try

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,19 +1,51 @@
 import path from 'path';
 
 export default {
-  devtool: 'eval',
-  entry: './src/index',
+  devtool: "eval",
+  entry: "./src/index",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/',
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/static/"
   },
   module: {
+    // module: {
+    //   rules: [
+    //   //   {
+    //   //   test: /\.css$/,
+    //   //   use: ["style-loader", "css-loader"],
+    //   // },
+    //     {
+    //       test: /\.m?js$/,
+    //       exclude: /(node_modules|bower_components)/,
+    //       use: {
+    //         loader: "babel",
+    //         options: {
+    //           presets: ["@babel/preset-env"]
+    //         }
+    //       }
+    //     }
+    //   //   , 
+    //   ]
+    // }
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       exclude: /node_modules/,
       include: __dirname,
     }],
-  },
+    // rules: [
+    //   {
+    //   test: /\.js$/,
+    //   loaders: ['babel'],
+    //   exclude: /node_modules/,
+    //   include: __dirname,
+    // },
+    //   {
+    //     test: /\.css$/,
+    //     use: ["style-loader", "css-loader"],
+
+    //   }
+    // ]
+  }
 };
